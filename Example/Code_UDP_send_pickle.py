fbe912b92b6c77b6ba3947e85b538cf550f3349a
Phayuth <70325209+Phayuth@users.noreply.github.com>
2021-01-30 19:11:32 +0700

    Update Code_UDP_send_pickle.py

--- Example/Code_UDP_send_pickle.py
+++ Example/Code_UDP_send_pickle.py
@@ -16,17 +16,17 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 t = 0
 
 while True:
-    tt = time.time()
-    delt= tt-t
-    Mb4P[6]=t
-    t = tt
+	tt = time.time()
+	delt= tt-t
+	Mb4P[6]=t
+	t = tt
 	Mb4P[0]=45
-    Mb4P[1]=0.5
-    Mb4P[2]=math.sqrt(1+delt)
-    Mb4P[3]=math.cos(delt)+math.sin(delt)
-    Mb4P[4]=12
-    Mb4P[5]=delt
-    Mb4P[7]=tt
+	Mb4P[1]=0.5
+	Mb4P[2]=math.sqrt(1+delt)
+	Mb4P[3]=math.cos(delt)+math.sin(delt)
+	Mb4P[4]=12
+	Mb4P[5]=delt
+	Mb4P[7]=tt
 	Message = pickle.dumps(Mb4P)      # pickling the message
 	sock.sendto(Message, (ip, port))  # send the data to the UDP
-    time.sleep(1)
+	time.sleep(0.01)
