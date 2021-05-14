68ea084604d4432c50a6e465a9ccd8a3b4a95cda
Phayuth <70325209+Phayuth@users.noreply.github.com>
2021-05-14 22:43:31 +0700

    Update nplinspace.py

--- Testout/nplinspace.py
+++ Testout/nplinspace.py
@@ -6,4 +6,13 @@ n = 100
 T = 1.
 
 times = np.linspace(0.,T,n)
-print(times)
+# print(times)
+
+
+# print(np.random.normal(0,0.1))
+
+
+# print(np.random.randn(2, 1))
+
+
+print((np.diag([1.0, np.deg2rad(30.0)]) ** 2) @ np.random.randn(2, 1))
